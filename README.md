e5452fd5a1f5f897f18c295198c452ff42a845b7
Tu Nombre <MIGHENRAM22@GMAIL.COM>
2025-04-03 11:41:43 -0500

    ciclo_while

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # estructuras_repetitivas_3147252
 PROYECTO PARA APRENDER LOS CICLOS FOR Y WHILE EN PYTHON
+
+## conceptos clave  ##
+
+
+**Breakpoint(punto de interrución)**: Herramienta
+para ejecutar (depurar) un programa 
+instruccion por instruccion, de tal manera
+que se pueda apreciar el comportamiento
+del programa bajo todas las condiciones posibles
+
+**variable contadora (contador):**
+una variable utilizada en ciclos
+debe inicializarse en 0 antes del ciclo
+
+* debe inicializarse en 0 antes del ciclo
+* por lo general se utilizan las letras i, j para nombrarlas
+* debe tener una **instruccion de incremenro**
+* la variable contadora debe participar en la condicional
+
+## Ciclo while:
+Es una estructura que permite repetir 
+Su bloque de codigo, **MIENTRAS QUE**
+su condicional sea verdadera
+-> si la condicioal tiene valor de 
+verdad false, ya dejan de repetirse 
+las instrucciones del bloque
+
+### sintaxis :
+
+```
+while condicional:
+INSTRUCCION 1
+INSTRUCCION 2 ...
+```
---
+++ ciclo_while.py
@@ -0,0 +1,10 @@
+#EJEMPLO 1:
+#Elabore un ciclo para
+#Mostrar los numeros de
+#1 al 10 en python:
+
+i = 0
+while i <= 5:
+    print(i)
+    i = i + 1   
+print ("fin de programa")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
